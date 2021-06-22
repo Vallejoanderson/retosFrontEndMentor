d0e9c910fd7973645688136aeedc0af4e5ff81e2
root <root@debianva.debianva>
2021-06-21 20:20:52 -0500

    Adding mobile version

--- faq-accordion-card-main/main.js
+++ faq-accordion-card-main/main.js
@@ -1,11 +1,14 @@
+/*Every question section is saved in questions*/
 const questions = document.querySelectorAll('.question');
 
+/*Open a forEach loop to catch every single question*/
 questions.forEach(function(question){
     const btn = question.querySelector('.icon');
     const currentQuestion = question.querySelector('h2');
     const prg = question.querySelector('p');
     btn.addEventListener('click', function(){
-	prg.classList.toggle('hide-para');
-	currentQuestion.classList.toggle('light-gray');
+        btn.classList.toggle('rotate');
+        currentQuestion.classList.toggle('light-gray');
+        prg.classList.toggle('hide-para');
     });
 });
