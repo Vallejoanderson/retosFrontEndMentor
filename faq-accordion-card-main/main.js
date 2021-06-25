128faa8aff7e731f1fc62faaa2d6cbea8c605e07
root <root@debianva.debianva>
2021-06-25 00:41:13 -0500

    building mobile version

--- faq-accordion-card-main/main.js
+++ faq-accordion-card-main/main.js
@@ -3,7 +3,7 @@ const questions = document.querySelectorAll('.question');
 
 /*Open a forEach loop to catch every single question*/
 questions.forEach(function(question){
-    const btn = question.querySelector('.icon');
+    const btn = question.querySelector('.icon, h2');
     const currentQuestion = question.querySelector('h2');
     const prg = question.querySelector('p');
     btn.addEventListener('click', function(){
