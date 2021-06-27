e8c7d8f5155d6010e89cf158f308bb3010e109dc
vallejoanderson <vallejoanderson@gmail.com>
2021-06-27 01:14:09 -0500

    padding and js reduction

--- article-preview-component-master/main.js
+++ article-preview-component-master/main.js
@@ -2,6 +2,5 @@ const btn = document.querySelector('.share-button');
 const bottom = document.querySelector('.bottom-section');
 
 btn.addEventListener('click', function(){
-	bottom.classList.add("show");
-	bottom.style.backgroundColor = "hsl(217, 19%, 35%)";
+	bottom.classList.toggle("show");
 });
