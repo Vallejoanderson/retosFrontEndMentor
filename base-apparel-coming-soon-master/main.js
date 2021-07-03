3de382319975197bf522261062c6d257922f60b9
vallejoanderson <vallejoanderson@gmail.com>
2021-07-03 00:52:41 -0500

    adding fuctions to button

---
+++ base-apparel-coming-soon-master/main.js
@@ -0,0 +1,16 @@
+const val = document.getElementById('email');
+const btn = document.querySelector('.button');
+const alert = document.querySelector('.alert');
+const alertMessage = document.querySelector('.alert__message');
+
+btn.addEventListener('click', function(){
+	if( val.value === 'Hola' )
+	{
+		alert.classList.remove('show-alert');
+		alertMessage.classList.remove('show-alert');
+	}
+	else{
+		alert.classList.add('show-alert');
+		alertMessage.classList.add('show-alert');
+	}
+});
