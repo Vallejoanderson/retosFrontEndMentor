ca29513a0d0f5b77870350a3fbc991fed4d32adf
vallejoanderson <vallejoanderson@gmail.com>
2021-07-07 23:14:08 -0500

    finishing js

--- intro-component-with-signup-form-master/main.js
+++ intro-component-with-signup-form-master/main.js
@@ -3,13 +3,24 @@ const fields = document.querySelectorAll('.register-section__data');
 
 fields.forEach( function(field) {
 	const field__content = field.querySelector('input');
-	const warning_text = field.querySelector('.warning');
+	const warning__text = field.querySelector('.warning');
 	btn.addEventListener('click', function(){
-		if (field_content !== null && field__content.value === ""){
-			warning__text.classList.add('show');
+		if( field__content.classList.contains('email')){ /*Check if we're in the email class*/
+			if( field__content.value.includes('@') && field__content.value.includes('.com'))
+			{
+				warning__text.classList.remove('show');
+			}
+			else{
+				warning__text.classList.add('show');
+			}
 		}
-		else{
-			warning__text.classList.remove('show');
+		else{ /* For other fields use this path */
+			if (field__content.value === ""){
+				warning__text.classList.add('show');
+			}
+			else{
+				warning__text.classList.remove('show');
+			}
 		}			
 	});
 });
