9d3d05336bc1fe966f80c400bc894e88b8a9c7cc
vallejoanderson <vallejoanderson@gmail.com>
2021-07-16 15:08:53 -0500

    setting js check value

--- ping-coming-soon-page-master/script/main.js
+++ ping-coming-soon-page-master/script/main.js
@@ -1,9 +1,12 @@
 const btn = document.querySelector('.notification-section__button');
 const error__message = document.querySelector('.error__message');
-const message = document.querySelector('.notification-section__email');
 
 btn.addEventListener('click', function(){
-	if( message.value === ''){
+	const message = document.querySelector('.notification-section__email');
+	if( message.value.includes('@') && message.value.includes('.com') ){
+		error__message.classList.remove('error__message--display-show');
+	}
+	else{
 		error__message.classList.add('error__message--display-show');
 	}
 });
