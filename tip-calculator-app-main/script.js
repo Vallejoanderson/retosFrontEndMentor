2dd52d056dcfecfc5202f1a4daf1140f2be6cd4f
vallejoanderson <vallejoanderson@gmail.com>
2021-07-27 14:57:22 -0500

    Reset button, reduction in bill__input click and reorganization of functions to the top

--- tip-calculator-app-main/script.js
+++ tip-calculator-app-main/script.js
@@ -4,19 +4,11 @@ const error = document.querySelector('.error__div');
 const btns = document.querySelectorAll('.bill__percentage');
 const tip = document.getElementById('tipamount');
 const total = document.getElementById('total');
-
-bill__input.addEventListener('input', function(){
-    let bill = bill__input.value;
-    let people = people__input.value;
-    if( people == 0 || people == ''){
-        error.classList.add('error__div-show');
-        people__input.addEventListener('input', evaluatePeople);
-    }
-});
+const reset = document.querySelector('.result__reset');
 
 function evaluatePeople(){ // Check number of people 
     people = people__input.value;
-    if( people == 0 ){
+    if( people == 0 || people == ''){
         error.classList.add('error__div-show');
     }
     else{
@@ -24,19 +16,58 @@ function evaluatePeople(){ // Check number of people
     }
 }
 
-btns.forEach(function(btn){ //Calculate percentages
-    btn.addEventListener('click', function(){
-        switch(btn.textContent){
-            case('5%'):
-                individualBill(15); 
-            break;
-        }
-    });
-});
-
-function individualBill(number){
+function individualBill(number = 1){ //Calculate tip and total bill for each person
     let bill = bill__input.value;
     let people = people__input.value;
     tip.textContent = `$${( number/100*bill/people ).toFixed(2)}`;
     total.textContent = `$${( (bill * (number/100+1))/people ).toFixed(2)}`;
 }
+
+bill__input.addEventListener('input', function(){
+    let bill = bill__input.value;
+    let people = people__input.value;
+    evaluatePeople();
+    people__input.addEventListener('input', evaluatePeople);
+});
+
+btns.forEach(function(btn){ //Calculate percentages
+    people = people__input.value;
+    btn.addEventListener('click', function(){
+        if( people != 0 && people != '' ) //Prevent to calculate if there is no people
+        {
+            switch(btn.textContent){
+                case('5%'):
+                    individualBill(5); 
+                    break;
+                case('10%'):
+                    individualBill(10); 
+                    break;
+                case('15%'):
+                    individualBill(15); 
+                    break;
+                case('25%'):
+                    individualBill(25); 
+                    break;
+                case('50%'):
+                    individualBill(50); 
+                    break;
+                default:
+                    btn.addEventListener('input', function(){
+                        individualBill(btn.value);
+                    });
+                    break;
+            }
+        }
+    });
+});
+
+reset.addEventListener('click', function(){ // Reset button
+    bill__input.value = '';
+    individualBill();
+    people__input.value = '';
+});
+
+
+
+
+
