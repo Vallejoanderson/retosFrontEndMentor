3248728837e8292c0490dac6b435cdcb7367ae4d
vallejoanderson <vallejoanderson@gmail.com>
2021-07-27 06:58:02 -0500

    Separiting first two basic fuctions

--- tip-calculator-app-main/script.js
+++ tip-calculator-app-main/script.js
@@ -1,18 +1,18 @@
 const bill__input = document.getElementById('bill');
 const people__input = document.getElementById('people');
 const error = document.querySelector('.error__div');
+const btns = document.querySelectorAll('.bill__percentage');
 
 bill__input.addEventListener('input', function(){
     let bill = bill__input.value;
     let people = people__input.value;
     if( people == 0 || people == ''){
         error.classList.add('error__div-show');
-        people__input.addEventListener('input', console.log('Hola'));
+        people__input.addEventListener('input', evaluatePeople);
     }
 });
 
-function evaluatePeople(){
-    console.log('Hola');
+function evaluatePeople(){ /* Check number of people */
     people = people__input.value;
     if( people == 0 ){
         error.classList.add('error__div-show');
