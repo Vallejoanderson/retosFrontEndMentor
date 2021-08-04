b286f73f57ac8c8258a8913232c14f46b4c0fa41
vallejoanderson <vallejoanderson@gmail.com>
2021-08-04 08:03:10 -0500

    Complete styled range input with progress

--- interactive-pricing-component-main/script/main.js
+++ interactive-pricing-component-main/script/main.js
@@ -11,3 +11,11 @@ checkbox.addEventListener('change', function(){
 		switchbox.style.backgroundColor = 'hsl(223, 50%, 87%)';
 	}
 });
+
+/*Slider progress*/
+for (let e of document.querySelectorAll('input[type="range"].slider-progress')) {
+  e.style.setProperty('--value', e.value);
+  e.style.setProperty('--min', e.min == '' ? '0' : e.min);
+  e.style.setProperty('--max', e.max == '' ? '100' : e.max);
+  e.addEventListener('input', () => e.style.setProperty('--value', e.value));
+}
