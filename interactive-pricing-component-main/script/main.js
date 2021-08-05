7e1ea13a7f4032be54b6bad55b9ed0fc56f3f9ff
vallejoanderson <vallejoanderson@gmail.com>
2021-08-05 07:54:11 -0500

    Adding JS functionalities for views and plans

--- interactive-pricing-component-main/script/main.js
+++ interactive-pricing-component-main/script/main.js
@@ -1,14 +1,33 @@
 const switchbox = document.querySelector('.switch');
 const checkbox = document.getElementById('checkbox');
+const views = document.querySelector('.views__amount-total');
+const price = document.querySelector('.fee__price-total');
+const slider = document.getElementById('slider');
+const time = document.querySelector('.fee__period');
+const minviews = 10;
+const minprice = 2;
+const discount = 0.25;
 
-/*Toggle background-color */
+views.textContent = minviews;
+price.textContent = minprice;
+
+slider.addEventListener('change', function(){
+	views.textContent = slider.value*minviews;
+	price.textContent = slider.value*minprice;
+});
+
+/* Toggle background-color */
 checkbox.addEventListener('change', function(){
 	if (checkbox.checked) {
 		console.log("I'm checked");
 		switchbox.style.backgroundColor = 'hsl(174, 86%, 45%)';
+		price.textContent = slider.value*minprice*12*discount;
+		time.textContent = ' /year';
 	}
 	else{
 		switchbox.style.backgroundColor = 'hsl(223, 50%, 87%)';
+		price.textContent = slider.value*minprice;
+		time.textContent = ' /month';
 	}
 });
 
