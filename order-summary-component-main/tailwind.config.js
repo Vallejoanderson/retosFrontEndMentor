ba317649dd1beec61bcd5b47b2563adb030edeea
vallejoanderson <vallejoanderson@gmail.com>
2021-08-28 23:38:00 -0500

    Applying bg for live preview

--- order-summary-component-main/tailwind.config.js
+++ order-summary-component-main/tailwind.config.js
@@ -6,8 +6,8 @@ module.exports = {
     },
     extend: {
       backgroundImage: (theme) => ({
-          'top-pattern': "url('/public/images/pattern-background-desktop.svg')",
-          'top-pattern-mb': "url('/public/images/pattern-background-mobile.svg')",
+          'top-pattern': "url('/images/pattern-background-desktop.svg')",
+          'top-pattern-mb': "url('/images/pattern-background-mobile.svg')",
         }),
       colors: {
         paleblue: 
